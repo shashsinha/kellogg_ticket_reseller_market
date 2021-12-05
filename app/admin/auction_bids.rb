5c143a591edaecfe3f222f9af7f82713f491b450
anonymous anonymous <shashwatssinha@gmail.com>
2021-12-05 21:13:36 +0000

    Whitelist columns in AuctionBid for ActiveAdmin

--- app/admin/auction_bids.rb
+++ app/admin/auction_bids.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AuctionBid do
 
+ permit_params :ticket_id, :bid_by_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
