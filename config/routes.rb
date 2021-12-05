82801fee068117c798e12341cddb270715268c6c
anonymous anonymous <shashwatssinha@gmail.com>
2021-12-05 21:13:27 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "tickets#index"
   resources :event_categories
   resources :auction_bids
